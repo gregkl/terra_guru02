12ecae1ea8ac74289f22f95697ecd27571982ce2
gregkl <gkluczynski@gmail.com>
2023-06-08 12:18:05 +0200

    adding vars

---
+++ variables.tf
@@ -0,0 +1,20 @@
+variable "TF_VAR_subscription_id" {
+type = string
+default = "f269d5c0-2323-43a9-9458-cf44cda92b6d"
+}
+
+variable "TF_VAR_tenant_id" {
+    type = string
+    default = "13e8d82d-3ac9-4d48-a6f8-eed8a2a2f30e"
+}
+
+variable "TF_VAR_agent_client_id" {
+    type = string
+    default = "13e8d82d-3ac9-4d48-a6f8-eed8a2a2f30e"
+    }
+
+variable "TF_VAR_agent_client_secret" {
+    type = string
+    sensitive = true
+}
+
